4f0c8f0f8c734e27183e1249613291f7ed1f1df1
shell_wataru <world.excite.8.enjoy@gmail.com>
2017-05-14 14:09:04 +0900

    arc072

---
+++ ARC072/D.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    // 整数の入力
+    long long N,D;
+    cin >> X >> Y;
+
+    if (abs(X - Y) <= 1){
+        cout << "Brown" << endl;
+    }else{
+        cout << "Alice" << endl;
+    }
+
+    return 0;
+}
